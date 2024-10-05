3eed27e5400f4d6ea6cf7932929e3e6e082853ca
Berychc <treewincsgo@gmail.com>
2024-10-05 15:01:06 +0300

    algorithms add

--- src/main/java/org/example/aglorithms/AlgorithmsWarmup.java
+++ src/main/java/org/example/aglorithms/AlgorithmsWarmup.java
@@ -26,4 +26,19 @@ public class AlgorithmsWarmup {
         return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
     }
 
+    /**
+     *
+     Given a string, take the first 2 chars and return the string with the 2 chars
+     added at both the front and back, so "kitten" yields"kikittenki".
+     If the string length is less than 2, use whatever chars are there.
+
+     front22("kitten") → "kikittenki"
+     front22("Ha") → "HaHaHa"
+     front22("abc") → "ababcab"
+     */
+    public String front22(String str) {
+        String front = str.length() < 2 ? str : str.substring(0, 2);
+        return front + str + front;
+    }
+
 }
